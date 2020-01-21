a4c6804ca11feeef6c0185da5bd8b6da859616f1
faniryxx <faniry597@gmail.com>
2020-01-21 11:06:14 +0100

    Graphes fonctionnels pour la référence

--- LED-Tester/referencewindow.cpp
+++ LED-Tester/referencewindow.cpp
@@ -1,12 +1,79 @@
 #include "referencewindow.h"
 #include "ui_referencewindow.h"
+#include <random>
 #include <qcustomplot.h>
+#include <QTimer>
 
 referenceWindow::referenceWindow(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::referenceWindow)
 {
     ui->setupUi(this);
+    ui->plotEfficacite->addGraph();
+    ui->plotEfficacite->graph(0)->setScatterStyle(QCPScatterStyle::ssCircle);
+    ui->plotEfficacite->graph(0)->setLineStyle(QCPGraph::lsLine);
+
+    ui->plotStabilite->addGraph();
+    ui->plotStabilite->graph(0)->setScatterStyle(QCPScatterStyle::ssCircle);
+    ui->plotStabilite->graph(0)->setLineStyle(QCPGraph::lsLine);
+
+    ui->plotTemperature->addGraph();
+    ui->plotTemperature->graph(0)->setScatterStyle(QCPScatterStyle::ssCircle);
+    ui->plotTemperature->graph(0)->setLineStyle(QCPGraph::lsLine);
+
+    ui->plotCouleurs->addGraph(); //rouge
+    ui->plotCouleurs->graph(0)->setScatterStyle(QCPScatterStyle::ssCircle);
+    ui->plotCouleurs->graph(0)->setLineStyle(QCPGraph::lsLine);
+    ui->plotCouleurs->graph(0)->setPen(QPen(QColor(250, 0, 0)));
+
+    ui->plotCouleurs->addGraph(); //vert
+    ui->plotCouleurs->graph(1)->setScatterStyle(QCPScatterStyle::ssCircle);
+    ui->plotCouleurs->graph(1)->setLineStyle(QCPGraph::lsLine);
+    ui->plotCouleurs->graph(1)->setPen(QPen(QColor(0, 200, 0)));
+
+    ui->plotCouleurs->addGraph(); //bleu
+    ui->plotCouleurs->graph(2)->setScatterStyle(QCPScatterStyle::ssCircle);
+    ui->plotCouleurs->graph(2)->setLineStyle(QCPGraph::lsLine);
+    ui->plotCouleurs->graph(2)->setPen(QPen(QColor(0, 0, 209)));
+
+    timerMesure = new QTimer(this);
+    timerTempsRestant = new QTimer(this);
+    connect(timerMesure,SIGNAL(timeout()),this,SLOT(updateGraph()));
+    connect(timerTempsRestant,SIGNAL(timeout()),this,SLOT(arreterTimer()));
+}
+
+void referenceWindow::ajouterPoint(double x, double y, QString param)
+{
+    if(param == "Efficacité")
+    {
+        efficacite_x.append(x);
+        efficacite_y.append(y);
+    }
+    else if(param == "Stabilité")
+    {
+        stabilite_x.append(x);
+        stabilite_y.append(y);
+    }
+    else if(param == "Température")
+    {
+        temperature_x.append(x);
+        temperature_y.append(y);
+    }
+    else if(param == "Rouge")
+    {
+        couleurs_x_R.append(x);
+        couleurs_rouge.append(y);
+    }
+    else if(param == "Vert")
+    {
+        couleurs_x_V.append(x);
+        couleurs_vert.append(y);
+    }
+    else if(param == "Bleu")
+    {
+        couleurs_x_B.append(x);
+        couleurs_bleu.append(y);
+    }
 }
 
 referenceWindow::~referenceWindow()
@@ -16,5 +83,135 @@ referenceWindow::~referenceWindow()
 
 void referenceWindow::on_mesurerRef_clicked()
 {
+    if(ui->intervalle->value()<=ui->duree->value())
+    {
+        nettoyerTout();
+        demarrerTimer();
+    }
+    else QMessageBox::critical(this, "Valeurs incorrectes", "Veuillez vérifier les valeurs et relancez la mesure.");
+}
 
+void referenceWindow::updateGraph()
+{
+    ajouterPoint((((ui->duree->value()+1)*1000)-timerTempsRestant->remainingTime())/1000,genererRandom(120,130),"Efficacité");
+    ajouterPoint((((ui->duree->value()+1)*1000)-timerTempsRestant->remainingTime())/1000,genererRandom(0,50),"Stabilité");
+    ajouterPoint((((ui->duree->value()+1)*1000)-timerTempsRestant->remainingTime())/1000,genererRandom(90,100),"Température");
+    ajouterPoint((((ui->duree->value()+1)*1000)-timerTempsRestant->remainingTime())/1000,genererRandom(0,255),"Rouge");
+    ajouterPoint((((ui->duree->value()+1)*1000)-timerTempsRestant->remainingTime())/1000,genererRandom(0,255),"Vert");
+    ajouterPoint((((ui->duree->value()+1)*1000)-timerTempsRestant->remainingTime())/1000,genererRandom(0,255),"Bleu");
+    dessiner("All");
+}
+
+void referenceWindow::arreterTimer()
+{
+    timerMesure->stop();
+    timerTempsRestant->stop();
+    QMessageBox::information(this, "Fin de la mesure", "Les mesures de référence ont été effectuées. Vous pouvez décider de refaire une mesure ou d'enregistrer les précédentes.");
+}
+
+void referenceWindow::effacer(QString param)
+{
+    if(param == "Efficacité")
+    {
+        efficacite_x.clear();
+        efficacite_y.clear();
+    }
+    else if(param == "Stabilité")
+    {
+        stabilite_x.clear();
+        stabilite_y.clear();
+    }
+    else if(param == "Température")
+    {
+        temperature_x.clear();
+        temperature_y.clear();
+    }
+    else if(param == "Couleurs")
+    {
+        couleurs_x_R.clear();
+        couleurs_x_V.clear();
+        couleurs_x_B.clear();
+        couleurs_rouge.clear();
+        couleurs_vert.clear();
+        couleurs_bleu.clear();
+    }
+}
+
+void referenceWindow::dessiner(QString param)
+{
+    if(param == "Efficacité")
+    {
+        ui->plotEfficacite->graph(0)->setData(efficacite_x,efficacite_y);
+        ui->plotEfficacite->rescaleAxes();
+        ui->plotEfficacite->replot();
+        ui->plotEfficacite->update();
+    }
+
+    else if(param == "Stabilité")
+    {
+        ui->plotStabilite->graph(0)->setData(stabilite_x,stabilite_y);
+        ui->plotStabilite->rescaleAxes();
+        ui->plotStabilite->replot();
+        ui->plotStabilite->update();
+    }
+
+    else if(param == "Température")
+    {
+        ui->plotTemperature->graph(0)->setData(temperature_x,temperature_y);
+        ui->plotTemperature->rescaleAxes();
+        ui->plotTemperature->replot();
+        ui->plotTemperature->update();
+    }
+
+    else if(param == "Couleurs")
+    {
+        ui->plotCouleurs->graph(0)->setData(couleurs_x_R,couleurs_rouge);
+        ui->plotCouleurs->graph(1)->setData(couleurs_x_V,couleurs_vert);
+        ui->plotCouleurs->graph(2)->setData(couleurs_x_B,couleurs_bleu);
+        ui->plotCouleurs->rescaleAxes();
+        ui->plotCouleurs->replot();
+        ui->plotCouleurs->update();
+    }
+
+    else if(param == "All")
+    {
+        dessiner("Efficacité");
+        dessiner("Stabilité");
+        dessiner("Température");
+        dessiner("Couleurs");
+    }
+}
+
+int referenceWindow::genererRandom(int min, int max)
+{
+    return (qrand() % ((max + 1) - min) + min);
+}
+
+void referenceWindow::demarrerTimer()
+{
+    timerMesure->start((ui->intervalle->value())*1000);
+    timerTempsRestant->start((ui->duree->value())*1000);
+}
+
+void referenceWindow::nettoyerTout()
+{
+    effacer("Efficacité");
+    dessiner("Efficacité");
+    effacer("Stabilité");
+    dessiner("Stabilité");
+    effacer("Température");
+    dessiner("Température");
+    effacer("Couleurs");
+    dessiner("Couleurs");
+}
+
+double referenceWindow::calculerMoyenne(QVector<double> parametre)
+{
+    double somme=0;
+    for(int x=0;x<parametre.count();x++)
+    {
+        somme = somme + parametre.at(x);
+    }
+    double moyenne = somme/parametre.count();
+    return moyenne;
 }
--- LED-Tester/referencewindow.h
+++ LED-Tester/referencewindow.h
@@ -13,13 +13,29 @@ class referenceWindow : public QWidget
 
 public:
     explicit referenceWindow(QWidget *parent = nullptr);
+    void ajouterPoint(double x, double y, QString param);
+    void effacer(QString param);
+    void dessiner(QString param);
+    int genererRandom(int min, int max);
+    void demarrerTimer();
+    void nettoyerTout();
+    double calculerMoyenne(QVector<double> parametre);
     ~referenceWindow();
 
 private slots:
     void on_mesurerRef_clicked();
+    void updateGraph();
+    void arreterTimer();
 
 private:
     Ui::referenceWindow *ui;
+    QVector<double> efficacite_x, efficacite_y;
+    QVector<double> stabilite_x, stabilite_y;
+    QVector<double> temperature_x, temperature_y;
+    QVector<double> couleurs_x_R, couleurs_x_V, couleurs_x_B, couleurs_rouge, couleurs_vert, couleurs_bleu;
+    QTimer *timerMesure;
+    QTimer *timerTempsRestant;
+    double efficacite, stabilite, temperature, rouge, vert, bleu;
 };
 
 #endif // REFERENCEWINDOW_H
